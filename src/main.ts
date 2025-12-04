42fddac6f723a1cd9b0976a32c426699ea175608
jsioneeee <jsiona@gbox.ncf.edu.ph>
2025-12-04 18:34:07 +0800

    change the vercel link

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,7 @@ async function bootstrap() {
     origin: [
       "http://localhost:3000",
       "http://192.168.56.1:3000",
-      "https://login-jy9j.vercel.app"
+      "https://login-yp6n.vercel.app"
     ],
     credentials: true,
   });
